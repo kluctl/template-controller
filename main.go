8ab2c3698b381555da567f7de124d8f0fbf870b5
Alexander Block <ablock84@gmail.com>
2022-09-09 13:40:52 +0200

    feat: Add status reporter stubs

--- main.go
+++ main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ResourceTemplate")
 		os.Exit(1)
 	}
+	if err = (&controllers.StatusReporterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StatusReporter")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
