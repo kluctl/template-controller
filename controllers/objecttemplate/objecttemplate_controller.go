3816474057d62456ee8e99808a199eae79af7c94
Alexander Block <ablock84@gmail.com>
2022-09-14 14:53:32 +0200

    chore: Allow to read namespaces

--- controllers/objecttemplate/objecttemplate_controller.go
+++ controllers/objecttemplate/objecttemplate_controller.go
@@ -49,6 +49,7 @@ type ObjectTemplateReconciler struct {
 //+kubebuilder:rbac:groups=templates.kluctl.io,resources=objecttemplates/finalizers,verbs=update
 //+kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch
 
 // Reconcile a resource
 func (r *ObjectTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
