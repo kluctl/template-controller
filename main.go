3be9a89e9d6ce45004474d9a93407c0195b213a3
Alexander Block <ablock84@gmail.com>
2022-11-29 16:32:57 +0100

    feat: Add stubs for GitProjector

--- main.go
+++ main.go
@@ -138,6 +138,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ListGithubPullRequests")
 		os.Exit(1)
 	}
+	if err = (&controllers.GitProjectorReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "GitProjector")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
