ab6abbc68dc56f3477127fe2c78f92d220250fc8
Alexander Block <ablock84@gmail.com>
2023-01-06 15:42:39 +0100

    refactor: Move some functionality of ObjectTemplateReconciler into new base class

---
+++ controllers/base_template_reconciler.go
@@ -0,0 +1,111 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	templatesv1alpha1 "github.com/kluctl/template-controller/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+	"sync"
+)
+
+type BaseTemplateReconciler struct {
+	client.Client
+	Scheme       *runtime.Scheme
+	FieldManager string
+
+	controller   controller.Controller
+	watchedKinds map[schema.GroupVersionKind]bool
+	mutex        sync.Mutex
+}
+
+func (r *BaseTemplateReconciler) getClientForObjects(serviceAccountName string, objNamespace string) (client.Client, error) {
+	restConfig, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	name := "default"
+	if serviceAccountName != "" {
+		name = serviceAccountName
+	}
+	if name == "" {
+		return nil, fmt.Errorf("empty serviceAccountName not allowed")
+	}
+	username := fmt.Sprintf("system:serviceaccount:%s:%s", objNamespace, name)
+	restConfig.Impersonate = rest.ImpersonationConfig{UserName: username}
+
+	c, err := client.New(restConfig, client.Options{Mapper: r.RESTMapper()})
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func (r *BaseTemplateReconciler) addWatchForKind(ctx context.Context, gvk schema.GroupVersionKind) error {
+	logger := log.FromContext(ctx)
+
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if r.watchedKinds == nil {
+		r.watchedKinds = map[schema.GroupVersionKind]bool{}
+	}
+
+	if x, ok := r.watchedKinds[gvk]; ok && x {
+		return nil
+	}
+
+	logger.V(1).Info("Starting watch for new kind", "gvk", gvk)
+
+	var dummy unstructured.Unstructured
+	dummy.SetGroupVersionKind(gvk)
+
+	err := r.controller.Watch(&source.Kind{Type: &dummy}, handler.EnqueueRequestsFromMapFunc(func(object client.Object) []reconcile.Request {
+		var list templatesv1alpha1.ObjectTemplateList
+		err := r.List(context.Background(), &list, client.MatchingFields{
+			forMatrixObjectKey: BuildObjectIndexValue(object),
+		})
+		if err != nil {
+			return nil
+		}
+		var reqs []reconcile.Request
+		for _, x := range list.Items {
+			reqs = append(reqs, reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Namespace: x.Namespace,
+					Name:      x.Name,
+				},
+			})
+		}
+		return reqs
+	}))
+	if err != nil {
+		return err
+	}
+
+	r.watchedKinds[gvk] = true
+	return nil
+}
+
+func (r *BaseTemplateReconciler) buildBaseVars(templateObj runtime.Object, objVarName string) (map[string]any, error) {
+	vars := map[string]any{}
+
+	u, err := runtime.DefaultUnstructuredConverter.ToUnstructured(templateObj)
+	if err != nil {
+		return nil, err
+	}
+
+	vars[objVarName] = u
+	return vars, nil
+}
