eb1d5355f157bf73e6eb752322a5c57a55aabde9
Alexander Block <ablock84@gmail.com>
2023-01-09 09:19:33 +0100

    feat: Add stubs for GitlabComment

--- main.go
+++ main.go
@@ -160,6 +160,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GitProjector")
 		os.Exit(1)
 	}
+	if err = (&controllers.GitlabCommentReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "GitlabComment")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
